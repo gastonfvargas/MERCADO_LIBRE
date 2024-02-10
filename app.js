6436bfe2806a67eea83d9f8dd4700e91d4b38aa4
gastonfvargas <gastonfvargas@gmail.com>
2024-02-10 20:08:22 -0300

    Solucionando errores de codigo

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const express = require ('express');
-const app = express();
-const path = require('path');
-const port = 3030;
+const express = require('express')
+const app = express()
+const path = require('path')
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
-app.get("/",(req,res) =>{
-    res.sendFile(path.join(__dirname,'/views/home.html'))
+app.listen(3000, ()=>{
+    console.log('Servidor Corriendo');
 })
 
-app.listen(port, () =>{
-    console.log('Servidor corriendo');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
 })
