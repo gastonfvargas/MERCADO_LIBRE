fde00bfbfa4136eaef23894e874642ee43fe382c
gastonfvargas <gastonfvargas@gmail.com>
2024-03-19 08:16:55 -0300

    Se modificaron los siguientes archivos para completar la septima entrega

---
+++ src/controllers/admin/store.controller.js
@@ -0,0 +1,23 @@
+const { loadData, saveData } = require("../../data");
+
+module.exports = (req, res) => {
+    const { name, price, discount, description, category } = req.body;
+    const image = req.file;
+    const products = loadData();
+
+    const newId = products[products.length - 1].id + 1;
+    const newProduct = {
+        id: newId,
+        name: name.trim(),
+        price: +price,
+        discount: +discount,
+        description: description.trim(),
+        category: category.trim(),
+        image: image ? image.filename: 'default-image.png'
+    }
+
+    products.push(newProduct)
+    saveData(products)
+
+    res.redirect(`/productos/detalle/${newId}`)
+}
